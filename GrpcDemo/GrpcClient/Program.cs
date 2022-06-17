fbeb9d26fc658237f87b80cfbb926a6bd96ca2d2
Nowocinski <vito24222@gmail.com>
2022-06-17 14:15:57 +0200

    Adding a connection to the grpc server

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.ReadLine();
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+var input = new HelloRequest { Name = "Rafał" };
+using var channel = GrpcChannel.ForAddress("https://localhost:7188/");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(input);
+Console.WriteLine(reply.Message);
+Console.ReadLine();
