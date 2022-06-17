6bd630ddacc017af5b3e0a62545c759a0db3d87f
Nowocinski <vito24222@gmail.com>
2022-06-17 15:07:16 +0200

    Added own service to grpc

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -1,9 +1,17 @@
 ﻿using Grpc.Net.Client;
 using GrpcService;
+using GrpcService.Protos;
 
-var input = new HelloRequest { Name = "Rafał" };
+//var input = new HelloRequest { Name = "Rafał" };
+//using var channel = GrpcChannel.ForAddress("https://localhost:7188/");
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(input);
+//Console.WriteLine(reply.Message);
+
 using var channel = GrpcChannel.ForAddress("https://localhost:7188/");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(input);
-Console.WriteLine(reply.Message);
+var customerClint = new Customer.CustomerClient(channel);
+var customerRequested = new CustomerLookupModel { UserId = 2 };
+var reply = await customerClint.GetCustomerInfoAsync(customerRequested);
+
+Console.WriteLine($"{reply.FirstName} {reply.LastName}");
 Console.ReadLine();
---
+++ GrpcDemo/GrpcService/Services/CustomersService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+
+namespace GrpcService.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        public readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
